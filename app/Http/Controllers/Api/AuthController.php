34086147dc564d6174f89cca6fc4fc1e926fd3cc
Himanshu Jaiswal <himanshu.mau1999@gmail.com>
2024-06-04 14:13:57 +0530

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
+            'password' => 'required|string|min:8',
         ]);
 
         if($validator->fails()){
